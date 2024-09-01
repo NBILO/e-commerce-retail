09ad193ea3c5427b99f185f9953d4adfe774ccfa
NBILO <nabilmgamat@gmail.com>
2024-09-02 01:06:01 +0200

    add mapper,service,repository and controler for payment entity

---
+++ services/payment-service/src/main/java/com/commerce/paymentservice/payment/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.commerce.paymentservice.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+
+}
